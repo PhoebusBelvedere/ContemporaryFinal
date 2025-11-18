bd8c10656925bb0b74c166fcb2f624e163f09d8f
Ian Wolfram <wolfrait@mail.uc.edu>
2025-11-18 13:13:54 -0500

    Created GroupMember class

---
+++ ContemporaryFinal/GroupMember.cs
@@ -0,0 +1,12 @@
+﻿namespace ContemporaryFinal
+{
+    public class GroupMember
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Program { get; set; }
+        public string SchoolYear { get; set; }
+
+    }
+}
--- ContemporaryFinal/Home.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ContemporaryFinal
-{
-    public class Home
-    {
-    }
-}
